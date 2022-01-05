00d339059768718660258a4e256adc4725b654b8
RobC <rob.clapham@gmail.com>
2022-01-05 11:07:47 +0000

    remove ROOT_DIR and definitions file

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,9 +7,12 @@ import numpy as np
 import pytest
 from fluentcheck import Is
 
-from definitions import ROOT_DIR
 from src.ecgai_training_data_physionet import models
-from src.ecgai_training_data_physionet.models import EcgRecord, EcgLeadRecord, DiagnosticCode
+from src.ecgai_training_data_physionet.models import (
+    EcgRecord,
+    EcgLeadRecord,
+    DiagnosticCode,
+)
 
 
 def module_logging_level():
@@ -21,7 +24,7 @@ def logger_name():
 
 
 def setup_test_record_data():
-    path = os.path.join(ROOT_DIR, "tests", "test_data", "00001_hr.json")
+    path = os.path.join("test_data", "00001_hr.json")
     with open(path) as json_file:
         data = json.load(json_file)
     record = EcgRecord.from_json(data)
@@ -37,7 +40,13 @@ def test_create_data_set_record(caplog):
         test_signal: List[float] = random_signals.tolist()
         test_lead = EcgLeadRecord.create("II", test_signal)
         test_leads = [test_lead]
-        record = EcgRecord.create(record_id=1, record_name="messageId", database_name="database", sample_rate=200, leads=test_leads)
+        record = EcgRecord.create(
+            record_id=1,
+            record_name="messageId",
+            database_name="database",
+            sample_rate=200,
+            leads=test_leads,
+        )
         assert record.sample_rate == 200
         assert type(record) is EcgRecord
 
@@ -51,8 +60,8 @@ def test_read_from_json(caplog):
 
 def test_create_description_code(caplog):
     with caplog.at_level(level=module_logging_level(), logger=logger_name()):
-        scp_code = 'DEF'
-        description = 'This is my class'
+        scp_code = "DEF"
+        description = "This is my class"
         sut = DiagnosticCode.create(scp_code=scp_code, description=description)
         Is(sut).of_type(DiagnosticCode)
         Is(sut.scp_code).not_empty.matches(scp_code)
